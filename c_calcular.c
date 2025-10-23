a30d0dff0a459da2070c191823264749e439c418
VELINDRO <110570945+VELINDRO@users.noreply.github.com>
2025-10-23 09:19:36 +0100

    função soma

--- c_calcular.c
+++ c_calcular.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+
+int suma(int n1,  int n2){
+    return n1 + n2;
+   
+
+    
+}
+
 int main(){
     int n1, n2;
     printf("Insira dois números inteiros separados por espaços: ");
     scanf("%d %d", &n1 , &n2);
     return 0;
 
+    int soma = suma(n1,n2);
+    printf("A soma dos dois valores é: %d", soma);
+    return 0;
 }
