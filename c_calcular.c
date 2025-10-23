675b4c8d020d945bf2575fafcd8d50029db7dff5
VELINDRO <110570945+VELINDRO@users.noreply.github.com>
2025-10-23 09:44:32 +0100

    função subtrair

--- c_calcular.c
+++ c_calcular.c
@@ -8,6 +8,10 @@ int suma(int n1,  int n2){
     
 }
 
+int sub(int n1, int n2){
+    return n1 - n2;
+
+}
 int main(){
     int n1, n2;
     printf("Insira dois números inteiros separados por espaços: ");
@@ -17,4 +21,12 @@ int main(){
     int soma = suma(n1,n2);
     printf("A soma dos dois valores é: %d", soma);
     return 0;
+
+    int subtrair = sub(n1,n2);
+    printf("A subtração dos dois valores é: %d", subtrair);
+    return 0;
+
+
+
 }
+
