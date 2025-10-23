3a1f3b3d9e4a7b920d4a82645bbb3bc7678cf13a
VELINDRO <110570945+VELINDRO@users.noreply.github.com>
2025-10-23 09:59:17 +0100

    função multiplicação

--- c_calcular.c
+++ c_calcular.c
@@ -3,30 +3,31 @@
 
 int suma(int n1,  int n2){
     return n1 + n2;
-   
 
-    
 }
 
 int sub(int n1, int n2){
     return n1 - n2;
 
 }
+int mult(int n1, int n2){ 
+    return n1 * n2 ;
+}
+
 int main(){
     int n1, n2;
     printf("Insira dois números inteiros separados por espaços: ");
     scanf("%d %d", &n1 , &n2);
-    return 0;
 
     int soma = suma(n1,n2);
-    printf("A soma dos dois valores é: %d", soma);
-    return 0;
+    printf("A soma dos dois valores é: %d\n", soma);
 
     int subtrair = sub(n1,n2);
-    printf("A subtração dos dois valores é: %d", subtrair);
+    printf("A subtração dos dois valores é: %d\n", subtrair);
+
+    int multiplicacao= mult(n1,n2);
+    printf("A divisão dosdois valores é: %2.f\n", multiplicacao);
+
     return 0;
-
-
-
 }
 
